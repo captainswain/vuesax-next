e53454c22e8564163749ab90c23d90cee46b2282
Shane Lindsay <shaneboom@gmail.com>
2020-09-19 03:33:57 -0700

    change filename

--- packages/vuesax/build/lib/webpack.umd.js
+++ packages/vuesax/build/lib/webpack.umd.js
@@ -1,26 +1,26 @@
-const path = require('path')
-const baseWebpackConfig = require('../webpack.base.js')
-const merge = require('webpack-merge')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const baseWebpackConfig = require("../webpack.base.js");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(baseWebpackConfig, {
   entry: {
-    'vuesax': './src/index.ts',
-    'vuesax.min': './src/index.ts',
+    vscustom: "./src/index.ts",
+    "vscsutom.min": "./src/index.ts"
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(process.cwd(), 'dist'),
-    publicPath: '/dist/',
-    library: 'Vuesax',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
+    filename: "[name].js",
+    path: path.resolve(process.cwd(), "dist"),
+    publicPath: "/dist/",
+    library: "vscustom",
+    libraryTarget: "umd",
+    libraryExport: "default",
     umdNamedDefine: true,
-    globalObject: 'typeof self !== \'undefined\' ? self : this'
+    globalObject: "typeof self !== 'undefined' ? self : this"
   },
-  plugins:[
+  plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: "[name].css"
     })
   ]
-})
+});
